6c406f2826360f06ec275a1ee0968cd80ef5ed9e
okbong <okbong@gmail.com>
2019-05-15 01:17:53 -0700

    Update closure.js

--- closure.js
+++ closure.js
@@ -1,3 +1,4 @@
+// Use access private variable inside function
 var name = "The Window";
 
 var object = {
@@ -12,5 +13,63 @@ var object = {
 
 };
 
-alert(object.getNameFunc()());
+console.log(object.getNameFunc()());
+　　
 
+
+function foo() 
+{ 
+			 var b = 1; 
+			 function inner(){ 
+				 return b; 
+			 } 
+			 return inner; 
+} 
+var get_func_inner = foo();		 
+
+console.log(get_func_inner()); 
+
+
+
+
+
+// Use as constructor initial value
+function foo1(outer_arg) { 
+  
+    function inner(inner_arg) { 
+        return outer_arg + inner_arg; 
+    } 
+    return inner; 
+} 
+var get_func_inner = foo1(5); 
+  
+console.log(get_func_inner(4)); 
+console.log(get_func_inner(3));
+
+
+
+
+
+// Outer function 
+function outer()  
+{ 
+    function create_Closure(val)  
+    { 
+        return function()  
+        { 
+            return val; 
+        } 
+    } 
+    var arr = []; 
+    var i; 
+    for (i = 0; i < 4; i++)  
+    { 
+        arr[i] = create_Closure(i); 
+    } 
+    return arr; 
+} 
+var get_arr = outer(); 
+console.log(get_arr[0]()); 
+console.log(get_arr[1]()); 
+console.log(get_arr[2]()); 
+console.log(get_arr[3]()); 
